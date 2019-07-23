02748800b3bc0d5ffc3ab7a30b2395c609149536
NajeraNet <najeranet.com@gmail.com>
2019-07-22 22:39:31 -0500

    FIXED (just for a moment) the problem becouse the preset plugin was removed :(

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,30 @@
+// var config = {
+// 	entry: './main.js',
+// 	output: {
+// 		path: __dirname + '/',
+// 		filename: 'index.js',
+// 	},
+// 	devServer: {
+// 		inline: true,
+// 		port: 8080
+// 	},
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.jsx?$/,
+// 				exclude: /node_modules/,
+// 				use: {
+// 					loader: 'babel-loader',
+// 				},
+// 			}
+// 		]
+// 	}
+// };
+
+// module.exports = config;
+
+//    -------   ---------   ---         -----       -----     ------    //
+
 const path = require('path');
     const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -14,12 +41,11 @@ const path = require('path');
       module: {
           rules: [
             {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
+              test: /\.jsx?$/,
+              exclude: /node_modules/,
+              use: {
                 loader: 'babel-loader',
-                query: {
-                  presets: ['es2015', 'react']
-                }
+              },
             }
           ]
       },
